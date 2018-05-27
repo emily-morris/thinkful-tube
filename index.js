cc1c9bfa7a4172983e0e7fc068acd7bd18c7e06d
Emily Morris <morris.emilyanne@gmail.com>
2018-05-27 14:33:02 -0500

    deleted example.js, added functions to display search results and submit form in index.js

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 //for adding pagination
-let search = '';
-let pageToken = '';
+// let search = '';
+// let pageToken = '';
 
-function getDataFromApi(searchTerm, pageToken = null) {
+function getDataFromApi(searchTerm, callback) {
 	const params = {
 		//type and structure of data
 		part: 'snippet',
 		//authorizes to get data
 		key: 'AIzaSyC3CvFT1jpGyiMXTEtXlX6Kn2z02NwIycU',
-		q: searchTerm,
+		q: searchTerm
 		//id of page of results
-		pageToken: pageToken
+		// pageToken: pageToken
 	}
 	$.getJSON(
 		YOUTUBE_SEARCH_URL, 
-		params, 
-		function(data){
-			let results = [];
-			console.log(data);
-			data.items.forEach(function(item) {
-				let resultString = renderResult(item);
-				results.push(resultString);
-			});
-		console.log(results);
-		}
+		params,
+		callback
 	);
-
 }
-getDataFromApi('cats',);
+
 function renderResult(result) {
 	return `
 		<div>
-			<img src="${item.snippet.thumbnails.default.url}">
+			<img src="${result.snippet.thumbnails.default.url}">
 		</div>
 		`;
 }
 
+function displayYouTubeSearchData(data) {
+	const results = data.items.map((item, index) =>
+		renderResult(item));
+	$('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+	$('.js-search-form').submit(event => {
+		event.preventDefault();
+		const queryTarget = $(event.currentTarget).find('.js-query');
+		const query = queryTarget.val();
+		//clear out the input
+		queryTarget.val("");
+		getDataFromApi(query, displayYouTubeSearchData);
+	});
+}
+
+$(watchSubmit);
