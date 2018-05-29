b1c9c3adb6595ccbeaf0b0c1883b27d3dbb02489
Emily Morris <morris.emilyanne@gmail.com>
2018-05-29 18:52:11 -0500

    cleaned up js code, centered paragraph text and increased font size

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-let data;
-let result;
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-
 //for adding pagination
-// let search = '';
-// let pageToken = '';
+	// let search = '';
+	// let pageToken = '';
 
 function getDataFromApi(searchTerm, callback) {
 	const params = {
 		//type and structure of data
 		part: 'snippet',
-		//authorizes to get data
+		//api key authorizes to get data
 		key: 'AIzaSyC3CvFT1jpGyiMXTEtXlX6Kn2z02NwIycU',
+		//query
 		q: searchTerm
 		//id of page of results
-		// pageToken: pageToken
+			// pageToken: pageToken
 	}
 	$.getJSON(
 		YOUTUBE_SEARCH_URL, 
@@ -24,7 +22,6 @@ function getDataFromApi(searchTerm, callback) {
 }
 
 function renderResult(result) {
-	result = result;
 	return `
 		<div class="lightbox-trigger" imgid="${result.id.videoId}">
 			<img src="${result.snippet.thumbnails.medium.url}">
@@ -33,10 +30,8 @@ function renderResult(result) {
 }
 
 function displayYouTubeSearchData(data) {
-	data = data;
 	const results = data.items.map((item, index) =>
 		renderResult(item));
-	console.log(data.items[0]);
 	$('.js-search-results').html(results);
 }
 
@@ -46,37 +41,32 @@ function watchSubmit() {
 		const queryTarget = $(event.currentTarget).find('.js-query');
 		const query = queryTarget.val();
 		//clear out the input
-		queryTarget.val("");
+		queryTarget.val('');
 		getDataFromApi(query, displayYouTubeSearchData);
 	});
 }
 
 $(watchSubmit);
 
-$(".js-search-results").on("click", ".lightbox-trigger", function() {
-	let video_id = $(this).attr("imgid");
-	console.log($(this));
-	console.log(video_id);
+$('.js-search-results').on('click', '.lightbox-trigger', function() {
+	let video_id = $(this).attr('imgid');
 	let image_href = $(this).html();
-	if($("#lightbox").length > 0) {
-    let url = "https://www.youtube.com/watch?v=" + $(this).attr("imgid");
-    console.log(url);
-		$("#content").html(image_href);
-		$(".js-result-name").attr("href", url);
-		$("#lightbox").show();
+	if($('#lightbox').length > 0) {
+    let url = "https://www.youtube.com/watch?v=" + $(this).attr('imgid');
+		$('#content').html(image_href);
+		$('.js-result-name').attr('href', url);
+		$('#lightbox').show();
 	} else {
-		console.log($(this));
-		console.log(video_id);
 		let lightbox =
 		`<div id="lightbox">
 			<p>Click image to play video</p>
 			<a class="js-result-name" href="https://www.youtube.com/watch?v=${video_id}" target="_blank"><div id="content">${image_href}
 			</div></a>
 		</div>`;
-		$("body").append(lightbox);
+		$('body').append(lightbox);
 	}
 });
 
-$('body').on('click', "#lightbox", function() {
+$('body').on('click', '#lightbox', function() {
 	$('#lightbox').hide();
 });
